cecd759e27fb2b774af9a7fb3912643713057b86
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-06-09 19:07:01 -0700

    make search bar dynamically resize

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,13 +3,21 @@
 const submitBtn = document.querySelector(".submit");
 
 /* FUNCTIONS */
-function getForecast(e) {
-    e.preventDefault();
+function getForecast(event) {
+    event.preventDefault();
+
+    // Get input city name and add to recently searched
     const city = document.querySelector("#input-city").value;
+    let recentCities = JSON.parse(localStorage.get("recentCities")) || [];
+    recentCities.push(city);
+    localStorage.get("recentCities", recentCities);
+
+    // Get weather data
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=73e3e96b49fb7822dadb93b5d873f6a4`)
         .then((response) => response.json())
         .then((response) => {
           console.log(response);
+
         },
         (error) => {alert("Error: " + error.message)}
       );
